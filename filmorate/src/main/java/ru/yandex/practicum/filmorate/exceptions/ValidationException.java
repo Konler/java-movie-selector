23a4d2c70a89c44e6835fe4e7acd53de943f76a5
Konler <yulya201195@mail.ru>
2023-02-14 13:32:19 +0300

    Ветка на ревью

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class ValidationException extends Exception{
-    public ValidationException(String message) {
-        super(message);
-    }
-}
