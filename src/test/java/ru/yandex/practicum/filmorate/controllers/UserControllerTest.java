ff6bda05b8d9a4ff85d5006d5e22bc876432d99d
Konler <yulya201195@mail.ru>
2023-02-25 00:47:26 +0300

    Коммит с добавленными файлами

--- src/test/java/ru/yandex/practicum/filmorate/controllers/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controllers/UserControllerTest.java
@@ -15,7 +15,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @SpringBootTest
 @AutoConfigureMockMvc
 class UserControllerTest {
